7d312d7c4c29c6d192c57a2e7bd443adbd2eedda
dw029 <56631871+dw029@users.noreply.github.com>
2019-12-11 15:00:30 -0500

    Finished binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,8 +2,28 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+    if (array.length === 1) {
+        if (array[0] === target) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    const midpoint = Math.floor(array.length/2)
+    const leftHalf = array.slice(0, midpoint)
+    const rightHalf = array.slice(midpoint)
+    if (target > array[midpoint-1]) {
+        return binarySearch(rightHalf, target)
+    } else {
+        return binarySearch(leftHalf, target)
+    }
+
 };
+
+//[1,3,4,5,10,15]
+// [5,10,15]
+// [15]
 
 /*
 	EXTRA CREDIT:
